8b0001153ed0ab06173f0726c51e077d7872978f
alejandro <aservet1@binghamton.edu>
2021-02-06 16:11:27 -0500

    progress!

---
+++ monitor/src/main/java/energyOracle/Monitor.java
@@ -0,0 +1,93 @@
+package DemoMavenProject.src.main.java.energyOracle;
+
+
+/*
+	credit due to: https://www.baeldung.com/java-timer-and-timertask
+
+*/
+
+import java.time.Instant;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import jRAPL.*;
+
+class DbSubmit extends TimerTask {
+	private static SyncEnergyMonitor monitor;
+	private static EnergyStats before, after;
+	static {
+		monitor.init();
+		after = monitor.getSample();
+	}
+	@Override
+	public void run() {
+		before = after;
+		System.out.println(EnergyDiff.between(before, after).dump());
+		after = monitor.getSample();
+	}
+}
+
+public class Monitor
+{
+	private int samplingRate;
+	private final SyncEnergyMonitor monitor;
+	private TimerTask dbSubmitTask;
+
+	public Monitor() {
+		monitor = new SyncEnergyMonitor();
+		samplingRate = 15000; // default every 15 seconds
+	}
+
+	public void init() {
+		monitor.init();
+	}
+
+	public void dealloc() {
+		monitor.dealloc();
+	}
+
+	private void writeToDB(EnergyDiff sample) {
+
+	}
+
+	private static void waitTill15() { // synchronize operations with every 15th second
+		while (Instant.now().getEpochSecond() % 15 != 0);
+		System.out.println(Instant.now());
+	}
+
+	public void startMonitoring(int ms) {
+//		// EnergyStats before, after = monitor.getSample();
+//		dbSubmitTask = new TimerTask() {
+//			EnergyStats before, after = monitor.getSample();
+//	
+//			public void run() {
+//				before = after;
+//				System.out.println(EnergyDiff.between(before, after).dump());
+//				after = monitor.getSample();
+//			}
+//		};
+
+		//DbSubmit dbSumbitTask = new DbSubmit();
+
+		System.out.println(monitor.getSample().dump());
+
+		Timer timer = new Timer("Timer");
+
+		long delay = 1000L;
+		long period = 1000L * 60L * 60L * 24L;
+		timer.scheduleAtFixedRate(new DbSubmit(), delay, period);
+	}
+
+	public void abortMonitoring() {
+
+	}
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		//for(int x = 0; x < 2; x++) { waitTill15(); Thread.sleep(1000); }
+		Monitor m = new Monitor();
+		m.init();
+		m.startMonitoring(10000);
+		m.dealloc();
+	}
+}
---
+++ monitor/src/main/java/energyOracle/Utils.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.time.Instant;
+
+import jRAPL.EnergySample;
+import jRAPL.EnergyStats;
+import jRAPL.EnergyDiff;
+import jRAPL.SyncEnergyMonitor;
+import jRAPL.ArchSpec;
+
+public class Utils {
+    public static HashMap<String, HashMap<String,Double>> toHashMap(EnergySample energy) {
+        HashMap<String, HashMap<String,Double>> M = new HashMap<String,HashMap<String,Double>>();
+        for (int socket = 1; socket <= ArchSpec.NUM_SOCKETS; socket++) {
+            HashMap<String,Double> m = new HashMap<String,Double>();
+            m.put("DRAM", energy.atSocket(1).getDram());
+            m.put("GPU", energy.atSocket(1).getGpu());
+            m.put("CORE", energy.atSocket(1).getCore());
+            m.put("PKG", energy.atSocket(1).getPackage());
+            M.put("Socket"+socket,m);
+        }
+        // m.put("timestamp", Math.floor((double)Instant.now().getEpochSecond()),);
+        return M;
+    }
+    public static void main(String... args) throws InterruptedException{
+        SyncEnergyMonitor m = new SyncEnergyMonitor();
+        m.init();
+     
+        EnergyStats before,after=m.getSample();
+     
+        for (int x = 0; x < 100 ; x++) {
+            before = after;
+            Thread.sleep(70);
+            after = m.getSample();
+            System.out.println(Utils.toHashMap(EnergyDiff.between(before,after)));
+        }
+
+        m.dealloc();
+    }
+}
